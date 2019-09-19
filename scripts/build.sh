5f875fc9437b2eb24256f7add4f13dbe42fed5fe
seyuf <mamadou@madit.fr>
2019-09-19 18:19:27 +0200

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ chmod +x bin/magento
 
 mysqladmin -h mysql -u root -pmagento status
 
-if[ $INPUT_ELASTICSUITE = 1 ]
+if [ $INPUT_ELASTICSUITE = 1 ]
 then
   bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation --es-hosts="elasticsearch:9200" --es-user="" --es-pass=""
 else
